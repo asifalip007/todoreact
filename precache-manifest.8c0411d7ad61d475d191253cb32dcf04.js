a636228ee8d978c8a13e73c53d2897714586a1f7
asifalip007 <asifaliputhukkolly@gmail.com>
2020-05-21 07:25:37 +0530

    Updates

---
+++ precache-manifest.8c0411d7ad61d475d191253cb32dcf04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15618caf35ccadb7ec89c68b896aa190",
+    "url": "/todoreact/index.html"
+  },
+  {
+    "revision": "ca1a0c65c4dac904a96b",
+    "url": "/todoreact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32977afbe4731cd7d138",
+    "url": "/todoreact/static/js/2.9550c93a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoreact/static/js/2.9550c93a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1a0c65c4dac904a96b",
+    "url": "/todoreact/static/js/main.8ee0a971.chunk.js"
+  },
+  {
+    "revision": "709418a21ad573f1fae5",
+    "url": "/todoreact/static/js/runtime-main.66181419.js"
+  }
+]);
